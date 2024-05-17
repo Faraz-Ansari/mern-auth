b2de1aff21f5102457c584b38beaefe6888db5d8
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-05-17 19:56:09 +0530

    installed react, tailwind css and created first template

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+            <h2 className='text-3xl text-red-600 text-center mt-4'>hello world</h2>
+        </div>
+    )
+}
+
+export default App
