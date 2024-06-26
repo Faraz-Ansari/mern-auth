ea710da67ae8d7ac3a03567a055f1925935e4f01
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-06-26 21:39:01 +0530

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then( () => {
+        console.log("Connected to database successfully");
+    })
+    .catch( (err) => {
+        console.error(err);
+    });
 
 const app = express();
 const PORT = 3000;
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
